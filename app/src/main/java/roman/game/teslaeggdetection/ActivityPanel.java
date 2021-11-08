c208ada746ab92e74ad8c1ab92048bfd6a98e664
Roman Michailov <roma321m@gmail.com>
2021-11-08 14:55:15 +0200

    - remove landscape mode option. - remove title bar and notification bar for a full screen mode.

--- app/src/main/java/roman/game/teslaeggdetection/ActivityPanel.java
+++ app/src/main/java/roman/game/teslaeggdetection/ActivityPanel.java
@@ -5,6 +5,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -41,7 +43,12 @@ public class ActivityPanel extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_panel);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_panel);
 
         findViews();
         setStartPics();
