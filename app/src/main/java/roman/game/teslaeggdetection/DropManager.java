5c2cc3b8325ef3a9a81f9fe1a0aeb568d5083337
Roman Michailov <roma321m@gmail.com>
2021-11-08 15:19:48 +0200

    - only one heart can be dropped at a time, can't be more than one in the activity at the same time even if the player doesn't have max lives.

--- app/src/main/java/roman/game/teslaeggdetection/DropManager.java
+++ app/src/main/java/roman/game/teslaeggdetection/DropManager.java
@@ -5,6 +5,7 @@ public class DropManager {
     private int lastRoad;
     private int lastType;
     private int livesPercentage, eggsPercentage, coinsPercentage;
+    private int heartInTheActivity;
 
     public DropManager() {
         setLastRoad(-1);
@@ -12,6 +13,11 @@ public class DropManager {
         setEggsPercentage(7);
         setCoinsPercentage(2);
         setLivesPercentage(1);
+        setHeartInTheActivity(0);
+    }
+
+    private void setHeartInTheActivity(int i) {
+        heartInTheActivity = i;
     }
 
     private void setLastRoad(int lastRoad) {
@@ -50,15 +56,19 @@ public class DropManager {
         // 1=egg, 2=coin, 3=live
         int total = livesPercentage + coinsPercentage + eggsPercentage;
         int percentage = getRandomNumber(1, total + 1);
-        if (livesLeft == ActivityPanel.MAX_LIVES) {
+        if (livesLeft == ActivityPanel.MAX_LIVES || heartInTheActivity > 0) {
+            if (heartInTheActivity > 0)
+                setHeartInTheActivity(heartInTheActivity-1);
             if (percentage <= eggsPercentage)
                 return EGG;
-            else return COIN;
+            else return COIN; // heart percentage goes to the coins
         }
         if (percentage <= eggsPercentage)
             return EGG;
         if (percentage <= eggsPercentage + coinsPercentage)
             return COIN;
+        // as long as the heart in the activity can't drop another one
+        setHeartInTheActivity(ActivityPanel.ITEMS);
         return LIVE;
     }
 
